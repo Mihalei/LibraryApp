8dd809e54d153ff4d61d3642f5db535911afe2bf
Petar <mihaljevic.petar@gmail.com>
2022-02-17 00:27:18 +0100

    create-react-app + brisanje nepotrebnog sadržaja + import bootstrap

---
+++ library/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        Library App
+      </header>
+    </div>
+  );
+}
+
+export default App;
